fdfaed77d1e0f3152573592442dec18f640a408a
Julius Breindl <jbreindl@Juliuss-MacBook-Air.local>
2022-04-17 16:01:57 -0400

    skeleton code written - video included for local testing. Took code from lab, see prnu.py for license

---
+++ extraction.py
@@ -0,0 +1,21 @@
+def extract(image, savepath):
+    """
+    Fits the image to the face.
+    Params:
+        image - the image to crop to face
+        savepath - where to save the frame
+    returns:
+        cropped image
+    """
+    pass
+
+
+def convert(path):
+    """
+    Converts a video to its frames
+    Params:
+        path - path to the video
+    returns:
+        returns list of frames of video
+    """
+    pass
